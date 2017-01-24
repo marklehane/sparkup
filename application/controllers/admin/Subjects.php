4c78942d2b3773bbcaef1725d5c94ce43775709a
Mark Lehane <mklehane@gmail.com>
2017-01-24 19:00:59 +0000

    Fixed issue with Subjects Controller

--- application/controllers/admin/Subjects.php
+++ application/controllers/admin/Subjects.php
@@ -1,51 +1,56 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Subjects extends CI_Controller {
-	public function index() {
-		$this->template->load('admin', 'default', 'subjects/index');
-	}
+class Subjects extends CI_Controller
+{
+    public function index()
+    {
+        $this->template->load('admin', 'default', 'subjects/index');
+    }
 
-	public function add() {
-		$this->form_validation->set_rules('name', 'Name', 'trim|required|min_length[3]');
+    public function add()
+    {
+        $this->form_validation->set_rules('name', 'Name', 'trim|required|min_length[3]');
 
-		if ($this->form_validation->run == FALSE) {
-			$this->template->load('admin', 'default', 'subjects/add');
-		} else {
-			// Create Post Array
-			$data = array(
-				'name' => $this->input->post('name')
-			);
-		}
+        if ($this->form_validation->run() == false) {
+            $this->template->load('admin', 'default', 'subjects/add');
+        } else {
+            // Create Post Array
+            $data = array(
+                'name' => $this->input->post('name'),
+            );
 
-		//Insert Subject
-		$this->Subject_model->add($data);
+            //Insert Subject
+            $this->Subject_model->add($data);
 
-		//Activity Array
-		$data = array(
-			'resource_id' => $this->db->insert_id(),
-			'type' => 'subjcet',
-			'action' => 'added',
-			'user_id' => 1,
-			'message' => 'A new subject was added ('.$data["name"].')'
-		);
+            //Activity Array
+            $data = array(
+                'resource_id' => $this->db->insert_id(),
+                'type'        => 'subject',
+                'action'      => 'added',
+                'user_id'     => 1,
+                'message'     => 'A new subject was added (' . $data["name"] . ')',
+            );
 
-		//Insert Activity
-		$this->Activity_model->add($data);
+            //Insert Activity
+            $this->Activity_model->add($data);
 
-		//Set Message
-		$this->session->set_flashdata('success', 'Subject has been added');
+            //Set Message
+            $this->session->set_flashdata('success', 'Subject has been added');
 
-		//Redirect
-		redirect('admin/subjects');
+            //Redirect
+            redirect('admin/subjects');
+        }
 
-	}
+    }
 
-	public function edit() {
-		$this->template->load('admin', 'default', 'subjects/edit');
-	}
+    public function edit()
+    {
+        $this->template->load('admin', 'default', 'subjects/edit');
+    }
 
-	public function delete() {
+    public function delete()
+    {
 
-	}
+    }
 }
--- application/models/activity_model.php
+++ application/models/activity_model.php
@@ -1,18 +1,22 @@
 <?php
 
-class Activity_model extends CI_MODEL {
-	
-	function __construct() {
-		parent::__construct();
-		$this->table = 'activities';
-	}
+class Activity_model extends CI_MODEL
+{
 
-	public function get_list() {
-		$query = $this->$this->db->get($this->table);
-		return $query->result();
-	}
+    public function __construct()
+    {
+        parent::__construct();
+        $this->table = 'activities';
+    }
 
-	public function add() {
-		$this->db->insert($this->table, $data);
-	}
+    public function get_list()
+    {
+        $query = $this->$this->db->get($this->table);
+        return $query->result();
+    }
+
+    public function add()
+    {
+        $this->db->insert($this->table, $data);
+    }
 }
