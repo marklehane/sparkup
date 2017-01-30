1bd0ad9f14d68a1b61646209c3bf6aa31f688b76
Mark Lehane <mklehane@gmail.com>
2017-01-30 23:15:41 +0000

    More work on pages

--- application/controllers/admin/Pages.php
+++ application/controllers/admin/Pages.php
@@ -10,18 +10,74 @@ class Pages extends CI_Controller
 
     public function add()
     {
-        $subject_options    = array();
-        $subject_options[0] = 'Select Subjects';
+        //Field Rules
+        $this->form_validation->set_rules('title', 'Title', 'trim|required|min_length[3]');
+        $this->form_validation->set_rules('subject_id', 'Subject', 'trim|required');
+        $this->form_validation->set_rules('body', 'Body', 'trim|required');
+        $this->form_validation->set_rules('is_published', 'Publish', 'required');
+        $this->form_validation->set_rules('is_featured', 'Feature', 'required');
+        $this->form_validation->set_rules('order', 'Order', 'integer');
 
-        $subject_list = $this->Subject_model->get_list();
+        if ($this->form_validation->run() == false) {
 
-        foreach ($subject_list as $subject) {
-            $subject_options[$subject->id] = $subject->name;
+            $subject_options    = array();
+            $subject_options[0] = 'Select Subjects';
+
+            $subject_list = $this->Subject_model->get_list();
+
+            foreach ($subject_list as $subject) {
+                $subject_options[$subject->id] = $subject->name;
+            }
+
+            $data['subject_options'] = $subject_options;
+
+            //Load Template
+            $this->template->load('admin', 'default', 'pages/add', $data);
+
+        } else {
+
+            $slug = str_replace(' ', '-', $this->input->post('title'));
+            $slug = strtolower($slug);
+
+            //Page Data
+            $data = array(
+                'title'        => $this->input->post('title'),
+                'slug'         => $slug,
+                'subject_id'   => $this->input->post('subject_id'),
+                'body'         => $this->input->post('body'),
+                'is_published' => $this->input->post('is_published'),
+                'is_published' => $this->input->post('is_published'),
+                'is_featured'  => $this->input->post('is_featured'),
+                'in_menu'      => $this->input->post('in_menu'),
+                // 'user_id'      => $this->session->userdata('user_id'),
+                'user_id'      => 1,
+                'order'        => $this->input->post('order'),
+
+            );
+
+            //Insert Page
+            $this->Page_model->add($data);
+
+            //Activity Array
+            $data = array(
+                'resource_id' => $this->db->insert_id(),
+                'type'        => 'page',
+                'action'      => 'added',
+                'user_id'     => 1,
+                'message'     => 'A new page was added (' . $data["title"] . ')',
+            );
+
+            //Insert Activity
+            $this->Activity_model->add($data);
+
+            //Set Message
+            $this->session->set_flashdata('success', 'Page has been added');
+
+            //Redirect
+            redirect('admin/pages');
+
         }
 
-        $data['subject_options'] = $subject_options;
-
-        $this->template->load('admin', 'default', 'pages/add', $data);
     }
 
     public function edit()
--- application/views/admin/pages/add.php
+++ application/views/admin/pages/add.php
@@ -1,59 +1,59 @@
 <h2 class="page-header">Add Page</h2>
-
+<?php echo validation_errors('<p class="alert alert-danger">'); ?>
 <?php echo form_open('admin/pages/add'); ?>
 	<div class="form-group">
-		<?php echo form_label('Page Name', 'name');?>
+		<?php echo form_label('Page Title', 'title'); ?>
 		<?php
-			$data = array(
-				'name' => 'name',
-				'id' => 'name',
-				'maxlength' => '100',
-				'class' => 'form-control',
-				'value' => set_value('name')
-			);
-		?>
+$data = array(
+    'name'      => 'title',
+    'id'        => 'title',
+    'maxlength' => '100',
+    'class'     => 'form-control',
+    'value'     => set_value('title'),
+);
+?>
 		<?php echo form_input($data); ?>
 	</div>
 
 	<!-- Page Subject -->
 	<div class="form-group">
 		<?php echo form_label('Subject', 'subject_id'); ?>
-		<?php echo form_dropdown('subject_id', $subject_options, 0, array('class' => 'form-control')); ?>			
+		<?php echo form_dropdown('subject_id', $subject_options, 0, array('class' => 'form-control')); ?>
 	</div>
 
 	<!-- Page Body -->
 	<div class="form-group">
 		<?php echo form_label('Body', 'body'); ?>
-		<?php 
-			$data = array(
-				'name' => 'body',
-				'id' => 'body',
-				'class' => 'form-control',
-				'value' => set_value('subject')
-			);
-		 ?>
+		<?php
+$data = array(
+    'name'  => 'body',
+    'id'    => 'body',
+    'class' => 'form-control',
+    'value' => set_value('subject'),
+);
+?>
 		 <?php echo form_textarea($data); ?>
 	</div>
-	
+
 	<!-- Publish -->
 	<div class="form-group">
-		<?php echo form_label('Published', 'is_publised'); ?>
-		<?php echo form_radio('is_publised', 1, TRUE) ?> Yes
-		<?php echo form_radio('is_publised', 0, FALSE) ?> No
+		<?php echo form_label('Published', 'is_published'); ?>
+		<?php echo form_radio('is_published', 1, true) ?> Yes
+		<?php echo form_radio('is_published', 0, false) ?> No
 	</div>
 
 	<!-- Feature -->
 	<div class="form-group">
 		<?php echo form_label('Feature', 'is_featured'); ?>
-		<?php echo form_radio('is_featured', 1, FALSE) ?> Yes
-		<?php echo form_radio('is_featured', 0, TRUE) ?> No
+		<?php echo form_radio('is_featured', 1, false) ?> Yes
+		<?php echo form_radio('is_featured', 0, true) ?> No
 	</div>
 
 	<!-- Menu -->
 	<div class="form-group">
 		<?php echo form_label('Add To Menu', 'in_menu'); ?>
-		<?php echo form_radio('in_menu', 1, TRUE) ?> Yes
-		<?php echo form_radio('in_menu', 0, FALSE) ?> No
+		<?php echo form_radio('in_menu', 1, true) ?> Yes
+		<?php echo form_radio('in_menu', 0, false) ?> No
 	</div>
 
 	<!-- Order -->
